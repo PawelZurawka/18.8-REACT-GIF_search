596851faff44d11e1b261ed7515f03724e72eca4
Paweł Żurawka <pzurawka@gmail.com>
2019-06-22 15:05:04 +0200

    Add api key

--- components/App.js
+++ components/App.js
@@ -1,5 +1,3 @@
-var prefix = "https://cors-anywhere.herokuapp.com/";
-
 App = React.createClass({
   getInitialState() {
     return {
@@ -7,6 +5,36 @@ App = React.createClass({
       searchingText: '',
       gif: {}
     }
+  },
+  handleSearch: function(searchingText) {  // 1.Pobierz na wejściu wpisywany tekst.
+    this.setState({
+      loading: true  // 2.Zasygnalizuj, że zaczął się proces ładowania.
+    });
+    this.getGif(searchingText, function(gif) {  // 3.Rozpocznij pobieranie gifa.
+      this.setState({  // 4 Na zakończenie pobierania:
+        loading: false,  // a przestań sygnalizować ładowanie,
+        gif: gif,  // b ustaw nowego gifa z wyniku pobierania
+        searchingText: searchingText  // c ustaw nowy stan dla wyszukiwanego tekstu
+      });
+    }.bind(this));
+  },
+  getGif: function(searchingText, callback) {
+    var GIPHY_API_URL = 'https://api.giphy.com';
+    var GIPHY_PUB_KEY = '9nHaixweXV90T40ZIvRDDwPLKtRy4FnT'
+    var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+           var data = JSON.parse(xhr.responseText).data;
+            var gif = {
+                url: data.fixed_width_downsampled_url,
+                sourceUrl: data.url
+            };
+            callback(gif);
+        }
+    };
+    xhr.send();
   },
   render: function() {
 
@@ -28,33 +56,5 @@ App = React.createClass({
          />
       </div>
     );
-  },
-  handleSearch: function(searchingText) {  // 1.Pobierz na wejściu wpisywany tekst.
-    this.setState({
-      loading: true  // 2.Zasygnalizuj, że zaczął się proces ładowania.
-    });
-    this.getGif(searchingText, function(gif) {  // 3.Rozpocznij pobieranie gifa.
-      this.setState({  // 4 Na zakończenie pobierania:
-        loading: false,  // a przestań sygnalizować ładowanie,
-        gif: gif,  // b ustaw nowego gifa z wyniku pobierania
-        searchingText: searchingText  // c ustaw nowy stan dla wyszukiwanego tekstu
-      });
-    }.bind(this));
-  },
-  getGif: function(searchingText, callback) {
-    var url = prefix + 'https://api.giphy.com/v1/' + '/v1/gifs/random?api_key=' + '9nHaixweXV90T40ZIvRDDwPLKtRy4FnT' + '&tag=' + searchingText;
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-           var data = JSON.parse(xhr.responseText).data;
-            var gif = {
-                url: data.fixed_width_downsampled_url,
-                sourceUrl: data.url
-            };
-            callback(gif);
-        }
-    };
-    xhr.send();
   },
 });
